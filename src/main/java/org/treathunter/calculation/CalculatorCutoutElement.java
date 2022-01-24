be65928710c7cd8c002ff79545367c23feb6950e
TreatHunter <michael.zanin.a@gmail.com>
2022-01-24 18:16:26 +0300

    bugfix area calculation in contsructor was in int i added double casting

--- src/main/java/org/treathunter/calculation/CalculatorCutoutElement.java
+++ src/main/java/org/treathunter/calculation/CalculatorCutoutElement.java
@@ -4,7 +4,7 @@ public class CalculatorCutoutElement {
 	public double area;
 	
 	public CalculatorCutoutElement(int lenght, int width) {
-		area = lenght * width / 1_000_000; //m^2
+		area =(double) lenght * width / 1_000_000; //m^2
 	}
 	
 	public CalculatorCutoutElement(double area) {
